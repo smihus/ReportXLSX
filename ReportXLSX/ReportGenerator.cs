dc435db515ebbf6deead3fbf10900d9e4eae6832
Mikhail Smirnov <mike-smirnoff@yandex.ru>
2017-10-01 18:25:42 +0400

    Инициализация

---
+++ ReportXLSX/ReportGenerator.cs
@@ -0,0 +1,91 @@
+﻿using ClosedXML.Excel;
+using System;
+
+namespace ReportXLSX
+{
+    public class ReportGenerator : IDisposable
+    {
+        #region Fields
+        XLWorkbook _template;
+        XLWorkbook _report;
+        string _templateFile;
+        string _templateWorksheetName;
+        string _reportFile;
+        #endregion
+
+        public ReportGenerator(string templateFile, string templateWorksheetName)
+        {
+            _templateFile = templateFile;
+            _templateWorksheetName = templateWorksheetName;
+        }
+
+        public ReportGenerator(string templateFile, string templateWorksheetName, string reportFile) : this(templateFile, templateWorksheetName)
+        {
+            _reportFile = reportFile;
+        }
+
+        public XLWorkbook Template
+        {
+            get
+            {
+                if (_template == null)
+                {
+                    _template = new XLWorkbook(_templateFile);
+                }
+
+                return _template;
+            }
+            set
+            {
+                _template = value;
+            }
+        }
+
+        public XLWorkbook Report
+        {
+            get
+            {
+                if (_report == null)
+                {
+                    _report = new XLWorkbook();
+                    _report.AddWorksheet("Report");
+                }
+
+                return _report;
+            }
+            set
+            {
+                _report = value;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_template != null)
+                _template.Dispose();
+
+            if (_report != null)
+                _report.Dispose();
+        }
+
+        public void InsertRange(string rangeName)
+        {
+            var templateWS = Template.Worksheet(_templateWorksheetName);
+            var table = templateWS.Table("Table1");
+            var range = table.HeadersRow();
+
+            var reportWS = Report.Worksheet("Report");
+            reportWS.Cell(1, 1).Value = range;
+        }
+
+        public void Save()
+        {
+            Report.SaveAs(_reportFile);
+        }
+
+        public void SaveAs(string reportFile)
+        {
+            Report.SaveAs(reportFile);
+        }
+    }
+}
---
+++ Tests.TestReport/TestReportGenerator.cs
@@ -0,0 +1,62 @@
+﻿using ClosedXML.Excel;
+using NUnit.Framework;
+using ReportXLSX;
+using System.Collections.Generic;
+
+namespace Tests.TestReport
+{
+    [TestFixture]
+    public class TestReportGenerator
+    {
+        ReportGenerator _sut;
+        private string _testDirectory;
+        List<object> _simpleDataList;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testDirectory = TestContext.CurrentContext.TestDirectory;
+
+
+
+
+            _simpleDataList = new List<object>();
+            _simpleDataList.Add(new { Company = "Company #1", Count = 10, Sum = 1000 });
+            _simpleDataList.Add(new { Company = "Company #2", Count = 20, Sum = 2000 });
+            _simpleDataList.Add(new { Company = "Company #3", Count = 30, Sum = 3000 });
+        }
+
+        [Test]
+        public void ShouldHasTemplateProperty()
+        {
+            var result = _sut.Template;
+
+            Assert.That(result, Is.TypeOf<XLWorkbook>());
+
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        public void ShouldCreateSimpleReport()
+        {
+            var templateFile = _testDirectory + @"\Templates\Template.xlsx";
+            var templateWorksheetName = "SimpleTemplate";
+            var reportFile = @"\SimpleReport.xlsx";
+
+            using (var sut = new ReportGenerator(templateFile, templateWorksheetName))
+            {
+                var range = "Head";
+                sut.InsertRange(range);
+
+                //foreach (var item in _simpleDataList)
+                //{
+                //    var itemRange = "Row";
+                //    sut.InsertRange(itemRange);
+                //}
+
+                reportFile = _testDirectory + reportFile;
+                sut.SaveAs(reportFile);
+            }
+        }
+    }
+}
